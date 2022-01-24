285b98342a2461ba03448072b2974f8e7da9bb47
Igor <joaooliveira.oliveira134@gmail.com>
2022-01-23 23:06:02 -0300

    Commit 230122, concluindo CRUD.

---
+++ js/buscar-pacientes.js
@@ -0,0 +1,41 @@
+var botaoAdicionar = document.querySelector("#buscar-paciente");
+
+botaoAdicionar.addEventListener("click", function () {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
+
+    xhr.addEventListener("load", function () {
+        var erroAjax = document.querySelector("#erro-ajax");
+        if (xhr.status == 200) {
+            erroAjax.classList.add("invisivel");
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta);
+
+            pacientes.forEach(function (paciente) {
+                adicionaPacienteNaTabela(paciente);
+            });
+        } else {
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+            erroAjax.classList.remove("invisivel");
+        }
+    });
+
+    xhr.addEventListener("load", function () {
+        var sucessoAjax = document.querySelector("#sucesso-ajax");
+        if (xhr.status == 200) {
+            sucessoAjax.classList.add("invisivel");
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta);
+
+            pacientes.forEach(function (paciente) {
+                adicionaPacienteNaTabela(paciente);
+                console.log(xhr.status);
+                console.log(xhr.responseText);
+                sucessoAjax.classList.remove("invisivel");
+            });
+        }
+    });
+
+    xhr.send();
+});
